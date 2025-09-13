3d75c71e308e0f9b13af561acb55d28e82712a0b
nonameebyrator985 <singheingemo@hotmail.com>
2025-09-13 23:58:10 +0900

    Add a brief description of application features in index.js

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
 // Point d'entrée principal pour l'application.
 // À développer selon les fonctionnalités nécessaires.
+// Fonctionnalités prévues : gestion des utilisateurs, panel d'administration, et tableau de bord.
